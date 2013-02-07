4b5befe2160125f11d4b3cf56652febce4f04514
Chandrasekar <cswamin@thoughtworks.com>
2013-02-07 22:09:09 +0530

    wip oauth

--- src/com/renttracker/MainActivity.java
+++ src/com/renttracker/MainActivity.java
@@ -1,10 +1,34 @@
 package com.renttracker;
 
+import android.accounts.*;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AccountManager accountManager = AccountManager.get(this);
+        Account account = accountManager.getAccountsByType("com.google")[0];
+        final Activity context = this;
+        accountManager.getAuthToken(account, "oauth2:https://www.googleapis.com/auth/drive", null, this, new AccountManagerCallback<Bundle>() {
+            @Override
+            public void run(AccountManagerFuture<Bundle> bundleAccountManagerFuture) {
+                try {
+                    String token = bundleAccountManagerFuture.getResult().getString(AccountManager.KEY_AUTHTOKEN);
+                    Log.e("TOKEN :: ", token);
+                } catch (OperationCanceledException e) {
+                    Log.e("EXCEPTION : ", e.getMessage(), e);
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    Log.e("EXCEPTION : ", e.toString(), e.getCause());
+                } catch (AuthenticatorException e) {
+                    Log.e("EXCEPTION : ", e.getMessage(), e);
+                }
+            }
+        }, null);
     }
 }
